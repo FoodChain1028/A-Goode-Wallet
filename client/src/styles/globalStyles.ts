c8a9c84fe561eb49ec91610258d61c18e3b76220
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2025-05-12 14:37:08 +0800

    add light theme, global styles for ThemeProvider

---
+++ client/src/styles/globalStyles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Nunito', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: ${props => props.theme.bg};
+    color: ${props => props.theme.fg};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  button,
+  input,
+  optgroup,
+  select,
+  textarea {
+    font-family: inherit; /* Or 'Nunito', sans-serif */
+    font-size: 100%;
+    line-height: 1.15;
+    margin: 0;
+  }
+`;
